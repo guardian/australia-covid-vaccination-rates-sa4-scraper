fixed col headers, removed % signs

geographic.py
@@ -42,7 +42,7 @@ def is_number(s):
         return False
 
 def cleanDF(df):
-	df.columns = ['State', 'SA4', 'At_least_one_dos_15', 'Fully_vaccinated_15']
+	df.columns = ['State', 'SA4', 'At_least_one_dose_15', 'Fully_vaccinated_15']
 	df = df.drop(df.index[0])
 	# df = df.replace('', np.nan)
 	# df = df.dropna()
@@ -69,6 +69,10 @@ for file in new_files:
 	dataframes.append(readPDF("pdfs/" + file))
 
 merge = pd.concat(dataframes, ignore_index=True)
-merge.to_csv('geographic_vax_rates.csv')	
+
+merge['At_least_one_dose_15'] = merge['At_least_one_dose_15'].str.rstrip('%').astype('float') /100
+merge['Fully_vaccinated_15'] = merge['Fully_vaccinated_15'].str.rstrip('%').astype('float') /100
+merge.to_csv('geographic_vax_rates.csv')
+
 
 print("Done. PDFs scraped")